645fcfef1176fa0a62d2eae5f80d937463ec3b96
ZineddineBk09 <eino1209@gmail.com>
2023-09-03 09:23:25 +0100

    fix

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Register from './src/Register';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Register from './src/Register'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
-        <Stack.Screen name="Register" component={Register} />
+      <Stack.Navigator initialRouteName='Register'>
+        <Stack.Screen name='Register' component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
